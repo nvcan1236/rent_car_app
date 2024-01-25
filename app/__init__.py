df017e4ebe7e8ea1fa537eb46e9ba0d4f51a3d3b
NguyenVanCanh <ngcanh1236@gmail.com>
2024-01-25 16:22:03 +0700

    add Procfile

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from urllib.parse import quote
 # from flask_login import LoginManager
@@ -7,7 +9,8 @@ import sqlite3
 
 app = Flask(__name__)
 app.secret_key = 'DUKFHAKDGHNAK,DFHLDSKIFHALSK,JGDN ,KDFAJBN'
-app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///site.db'
+# app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///site.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 # login = LoginManager(app=app)
 db = SQLAlchemy(app=app)
